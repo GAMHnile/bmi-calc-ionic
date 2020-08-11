d18b1d0106cfb272560f6e0fcdf1b3c076f133f4
GAMHnile <georgeamhuweh@gmail.com>
2020-08-11 15:26:26 +0100

    added android capacitor app, modified BmiResult

--- src/components/BmiResult.tsx
+++ src/components/BmiResult.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { IonRow, IonCol, IonCard, IonCardContent } from "@ionic/react";
 
-const BmiResult: React.FC<{ value: number | string }> = ({ value }) => (
+const BmiResult: React.FC<{ value: number }> = ({ value }) => (
   <IonRow>
     <IonCol>
       <IonCard>
-        <IonCardContent>
-          <h2>{value}</h2>
+        <IonCardContent className="ion-text-center" >
+          <h2>Your body mass index is: </h2>
+          <h3>{value.toFixed(2)}</h3>
         </IonCardContent>
       </IonCard>
     </IonCol>
